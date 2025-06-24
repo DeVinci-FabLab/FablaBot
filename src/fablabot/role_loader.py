9a7e0ff9d318cbc91eda0708f403e207d8ff9a84
austin <eliottroussille@gmail.com>
2025-06-24 17:34:18 +0200

    🚑 fix can_assign_role computation

--- src/fablabot/role_loader.py
+++ src/fablabot/role_loader.py
@@ -26,12 +26,19 @@ def load_roles(path: Path = ROLE_FILE) -> dict[str, dict[str, int]]:
 def can_assign_role(member: discord.Member, target_role: discord.Role, roles: dict[str, dict[str, int]]) -> bool:
     """Check if `member` can assign `target_role`."""
     target_role_id = roles[target_role.name]["id"]
-    member_permissions_integers = (roles[role.name]["permissions_integer"] for role in member.roles if role.name in roles)
+    member_permissions_integers = [roles[role.name]["permissions_integer"] for role in member.roles if role.name in roles]
+    logger.debug(
+        "Checking if member %s with permissions integers: %s can assign role %s (ID: %d)",
+        member.name,
+        member_permissions_integers.__str__().replace(",", ", "),
+        target_role.name,
+        target_role_id,
+    )
     for permission_integer in member_permissions_integers:
-        if permission_integer == 8189 or (target_role_id % 2 * permission_integer) // permission_integer == 1:
+        if permission_integer == 8189 or (permission_integer % (2 * target_role_id)) // target_role_id == 1:
             return True
     logger.debug(
-        "Member %s cannot assign role %s: insufficient permissions",
+        "Member %s cannot assign role %s: insufficient permissions.",
         member.name,
         target_role.name,
     )
